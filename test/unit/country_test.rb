7a3d6721bce2c7953a1a1c24704740eed3815f4a
James Smith <james@marvin.(none)>
2008-06-24 06:44:02 +0100

    added tests

--- test/unit/country_test.rb
+++ test/unit/country_test.rb
@@ -3,4 +3,26 @@ require File.dirname(__FILE__) + '/../test_helper'
 class CountryTest < Test::Unit::TestCase
   fixtures :countries
 
+  def test_validation_failure
+    country = Country.new
+    assert country.valid? == false
+    country = Country.new(:name => "test")
+    assert country.valid? == false
+    country = Country.new(:abbreviation => "test")
+    assert country.valid? == false
+  end
+
+  def test_validation_success
+    country = Country.new(:name => "test", :abbreviation => "test")
+    assert country.valid? == true
+  end
+
+  def test_list
+    list = Country.list
+    assert list.size == 3
+    assert list[0].name == "-- Other --"
+    assert list[1].name == "United Kingdom"
+    assert list[2].name == "United States"
+  end
+
 end
