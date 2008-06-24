87f32bd88f22a55d375c2542ee40f4983177b4a1
James Smith <james@marvin.(none)>
2008-06-24 06:43:47 +0100

    added tests

--- test/unit/flight_class_test.rb
+++ test/unit/flight_class_test.rb
@@ -3,4 +3,18 @@ require File.dirname(__FILE__) + '/../test_helper'
 class FlightClassTest < Test::Unit::TestCase
   fixtures :flight_classes
 
+  def test_validation_failure
+    flight_class = FlightClass.new()
+    assert flight_class.valid? == false
+    flight_class = FlightClass.new(:name => "First class")
+    assert flight_class.valid? == false
+    flight_class = FlightClass.new(:scale_factor => 3.4)
+    assert flight_class.valid? == false
+  end
+
+  def test_validation_success
+    flight_class = FlightClass.new(:name => "First class", :scale_factor => 3.4)
+    assert flight_class.valid? == true
+  end
+
 end
