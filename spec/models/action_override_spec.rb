85e2395be190b03399c178d9371fd3603cb5c62c
James Smith <james@floppy.org.uk>
2008-11-04 23:02:03 +0000

    Add some rspec tests

--- spec/models/action_override_spec.rb
+++ spec/models/action_override_spec.rb
@@ -11,16 +11,27 @@ describe ActionOverride do
     @override.action.should == actions(:turn_off_lights)
   end
 
-  it "is specific to a particular country" do    
-    @override.country.should_not be_nil
+  it "can be specific to a particular country" do    
+    @override.should respond_to(:country)
   end
 
   it "can be paid for" do    
-    @override.paid_for.should_not be_nil
+    @override.should respond_to(:paid_for)
   end
 
   it "has some content" do
     @override.content.should_not be_nil
   end
 
+  it "must have content and related action defined to be valid" do
+    x = ActionOverride.create
+    x.should_not be_valid
+    x = ActionOverride.create(:content => "test")
+    x.should_not be_valid
+    x = ActionOverride.create(:action_id => 1)
+    x.should_not be_valid
+    x = ActionOverride.create(:content => "test", :action_id => 1)
+    x.should be_valid
+  end  
+  
 end
