c142d9bf57f173ff834b890e6c4c301996a9b70d
James Smith <james@Marvin.(none)>
2009-11-30 22:32:06 +0000

    comments require users

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, :polymorphic => true  
-  validates_presence_of :commentable_id, :commentable_type, :text
+  validates_presence_of :commentable_id, :commentable_type, :text, :user
   attr_accessible :text
 end
