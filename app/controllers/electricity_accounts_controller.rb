2ba4b7f860a668c667c2d0024d2901709c3c778e
James Smith <james@Marvin.(none)>
2010-01-17 20:46:32 +0000

    make electricity account controller resourceful

--- app/controllers/electricity_accounts_controller.rb
+++ app/controllers/electricity_accounts_controller.rb
@@ -1,35 +1,60 @@
-class ElectricityAccountsController < AuthenticatedController
+class ElectricityAccountsController < BelongsToUser
+  before_filter :get_account, :except => [:index, :new, :create]
+
   def index
     redirect_to :controller => '/data_entry/index', :action => 'edit_accounts'
   end
 
-  def edit
-    @electricity_account = params[:id] ? @current_user.electricity_accounts.find_by_id(params[:id]) : ElectricityAccount.new
-    @electricity_suppliers = @current_user.country.electricity_suppliers.find( :all, :order => 'name')
-    @electricity_units = ElectricityUnit.find( :all, :order => 'name')
-    unless request.post? or params[:id]
-      @electricity_account.electricity_supplier = ElectricitySupplier.default(@current_user.country)
-      @electricity_account.electricity_unit = @current_user.country.electricity_unit
-    end
-    if request.post?
-      @electricity_account.user = @current_user
-      @electricity_account.update_attributes(params[:electricity_account])
-      @current_user.update_stored_statistics!
-      redirect_to :controller => '/data_entry/electricity', :account => @electricity_account  if @electricity_account.save
-    end
-    # Set page name
-    if params[:id]
-      @pagename = "Editing " + @electricity_account.name
+  def new
+    @account = ElectricityAccount.new
+    # Set default values
+    @account.electricity_supplier = ElectricitySupplier.default(@current_user.country)
+    @account.electricity_unit = @current_user.country.electricity_unit
+    # Get options for select
+    get_select_options
+  end
+
+  def create
+    @account = @user.electricity_accounts.create(params[:electricity_account])
+    if @account.save
+      @user.update_stored_statistics!
+      redirect_to :controller => '/data_entry/electricity', :account => @account
     else
-      @pagename = "Add electricity account"
+      get_select_options
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    get_select_options
+  end
+
+  def update
+    @account.update_attributes!(params[:electricity_account])
+    if @account.save
+      @user.update_stored_statistics!
+      redirect_to :controller => '/data_entry/electricity', :account => @account
+    else
+      get_select_options
+      render :action => 'edit'
     end
   end
 
   def destroy
-    account = @current_user.electricity_accounts.find_by_id(params[:id])
-    account.destroy if not account.nil?
-    @current_user.update_stored_statistics!
-    index
+    @account.destroy
+    @user.update_stored_statistics!
+    redirect_to :controller => '/data_entry/index', :action => 'edit_accounts'
+  end
+
+protected
+
+  def get_select_options
+    @electricity_suppliers = @current_user.country.electricity_suppliers.find( :all, :order => 'name')
+    @electricity_units = ElectricityUnit.find( :all, :order => 'name')
+  end
+
+  def get_account
+    @account = @current_user.electricity_accounts.find(params[:id])
   end
 
 end
