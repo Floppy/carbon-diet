074c321ccf36c18e1744c28ce15927f8cd0e80a4
James Smith <james@Marvin.(none)>
2010-01-17 20:59:45 +0000

    make gas account controller resourceful

--- app/controllers/gas_accounts_controller.rb
+++ app/controllers/gas_accounts_controller.rb
@@ -1,35 +1,60 @@
-class GasAccountsController < AuthenticatedController
+class GasAccountsController < BelongsToUser
+  before_filter :get_account, :except => [:index, :new, :create]
+
   def index
     redirect_to :controller => '/data_entry/index', :action => 'edit_accounts'
   end
 
-  def edit
-    @gas_account = params[:id] ? @current_user.gas_accounts.find_by_id(params[:id]) : GasAccount.new
-    @gas_suppliers = @current_user.country.gas_suppliers.find( :all, :order => 'name')
-    @gas_units = GasUnit.find( :all, :order => 'name')
-    unless request.post? or params[:id]
-      @gas_account.gas_supplier = GasSupplier.default(@current_user.country)
-      @gas_account.gas_unit = @current_user.country.gas_unit
-    end
-    if request.post?
-      @gas_account.user = @current_user
-      @gas_account.update_attributes(params[:gas_account])
-      @current_user.update_stored_statistics!
-      redirect_to :controller => '/data_entry/gas', :account => @gas_account if @gas_account.save
-    end
-    # Set page name
-    if params[:id]
-      @pagename = "Editing " + @gas_account.name
+  def new
+    @account = GasAccount.new
+    # Set default values
+    @account.gas_supplier = GasSupplier.default(@current_user.country)
+    @account.gas_unit = @current_user.country.gas_unit
+    # Get options for select
+    get_select_options
+  end
+
+  def create
+    @account = @user.gas_accounts.create(params[:gas_account])
+    if @account.save
+      @user.update_stored_statistics!
+      redirect_to :controller => '/data_entry/gas', :account => @account
     else
-      @pagename = "Add gas account"
+      get_select_options
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    get_select_options
+  end
+
+  def update
+    @account.update_attributes!(params[:gas_account])
+    if @account.save
+      @user.update_stored_statistics!
+      redirect_to :controller => '/data_entry/gas', :account => @account
+    else
+      get_select_options
+      render :action => 'edit'
     end
   end
 
   def destroy
-    account = @current_user.gas_accounts.find_by_id(params[:id])
-    account.destroy if not account.nil?
-    @current_user.update_stored_statistics!
-    index
+    @account.destroy
+    @user.update_stored_statistics!
+    redirect_to :controller => '/data_entry/index', :action => 'edit_accounts'
+  end
+
+protected
+
+  def get_select_options
+    @gas_suppliers = @current_user.country.gas_suppliers.find( :all, :order => 'name')
+    @gas_units = GasUnit.find( :all, :order => 'name')
+  end
+
+  def get_account
+    @account = @current_user.gas_accounts.find(params[:id])
   end
 
 end
