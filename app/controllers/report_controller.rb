0dc62379ce1230d0e15daf4861a0a509d7eeb353
James Smith <james@Marvin.(none)>
2010-01-17 16:30:17 +0000

    added basic analysis to iphone app

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,26 +1,36 @@
 class ReportController < AuthenticatedController  
 
   def index
-    @total = 0.0
-    for item in @current_user.all_emissions
-      # Calculate total
-      temp = item[:data].calculate_total
-      # Add up data
-      @total += temp[:total]
+    respond_to do |format|
+      format.html {
+        @total = 0.0
+        for item in @current_user.all_emissions
+          # Calculate total
+          temp = item[:data].calculate_total
+          # Add up data
+          @total += temp[:total]
+        end
+        # Calculate daily and yearly amounts based on total
+        days = Date::today - @current_user.date_of_first_data
+        @perday = @total / days
+        @perannum = @perday * 365
+        # Calculate litres per day... using 540L per kg
+        @litres_per_day = @perday * 540
+        # Calculate balloons per day... using a 2 litre balloon
+        @balloons = @litres_per_day / 2
+        @seconds_per_balloon = 86400 / @balloons
+        # Compared the the amount you breathe out... based on 432L of CO2 per day breathed out
+        @times_breathed_out = @litres_per_day / 432
+        # Set page name
+        @pagename = "View analysis"
+      }
+      format.iphone {
+        days = Date::today - @current_user.date_of_first_data
+        days = 365 if days > 365
+        @totals = @current_user.calculate_totals(days)
+        render :layout => false
+      }
     end
-    # Calculate daily and yearly amounts based on total
-    days = Date::today - @current_user.date_of_first_data
-    @perday = @total / days
-    @perannum = @perday * 365
-    # Calculate litres per day... using 540L per kg
-    @litres_per_day = @perday * 540
-    # Calculate balloons per day... using a 2 litre balloon
-    @balloons = @litres_per_day / 2
-    @seconds_per_balloon = 86400 / @balloons
-    # Compared the the amount you breathe out... based on 432L of CO2 per day breathed out
-    @times_breathed_out = @litres_per_day / 432
-    # Set page name
-    @pagename = "View analysis"
   end
 
   def ratio
