6ef499d3fb9ee0ea85d0266ac6359bbaa368bfc0
James Smith <james@Marvin.(none)>
2010-02-05 12:21:04 +0000

    fix mailer problems

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,60 +1,46 @@
 class UserMailer < ActionMailer::Base
 
   def reminder(email, sent_at = Time.now)
-    setup(sent_at)
-    @subject     = 'A reminder from the Carbon Diet'
-    @recipients  = email
+    setup('A reminder from the Carbon Diet', email, sent_at)
   end
 
   def password_change(email, url, sent_at = Time.now)
-    setup(sent_at)
-    @subject        = 'Carbon Diet: Password change request'
-    @body["url"]    = url
-    @recipients     = email
+    setup('Carbon Diet: Password change request', email, sent_at)
+    body :url => url
   end
 
   def group_invitation(group, user, sent_at = Time.now)
-    setup(sent_at)
-    @subject       = 'Carbon Diet: Group invitation'
-    @body["group"] = group
-    @recipients    = user.email
+    setup('Carbon Diet: Group invitation', user.email, sent_at)
+    body :group => group
   end
 
   def friend_request(name, email, sent_at = Time.now)
-    setup(sent_at)
-    @subject     = 'Carbon Diet: Friend request'
-    @body["name"] = name
-    @recipients  = email
+    setup('Carbon Diet: Friend request', email, sent_at)
+    body :name => name
   end
 
   def comment_notification(user, commenter, sent_at = Time.now)
-    setup(sent_at)
-    @subject      = 'Carbon Diet: Someone wrote a comment on your profile!'
-    @body["from"] = commenter.name
-    @recipients   = user.email
+    setup('Carbon Diet: Someone wrote a comment on your profile!', user.email, sent_at)
+    body :commenter => commenter.name
   end
 
   def email_confirmation(user, sent_at = Time.now)
-    setup(sent_at)
-    @subject      = 'Carbon Diet: Please confirm your email address'
-    @body["code"] = user.confirmation_code
-    @recipients   = user.email
+    setup('Carbon Diet: Please confirm your email address', user.email, sent_at)
+    body :code => user.confirmation_code
   end
 
   def friend_invitation(from, email, group, sent_at = Time.now)
-    setup(sent_at)
-    @subject       = 'An invitation to join The Carbon Diet'
-    @body["from"]  = from
-    @body["group"] = group
-    @recipients    = email
+    setup('An invitation to join The Carbon Diet', email, sent_at)
+    body :sender => from, :group => group
   end
 
 private
 
-  def setup(sent_at)   
-    @from            = 'info@carbondiet.org'
-    @sent_on         = sent_at
-    @headers         = {}
+  def setup(subj, email, sent_at)
+    subject subj
+    recipients email
+    from 'info@carbondiet.org'
+    sent_on sent_at
   end
 
 end
