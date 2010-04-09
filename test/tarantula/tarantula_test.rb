e8a7dad2d032c0ec72a33fa456640e027d0fe66c
James Smith <james@floppy.org.uk>
2010-04-09 14:29:34 +0100

    create separate readonly, nondestructive, and full tarantula tests

--- test/tarantula/tarantula_test.rb
+++ test/tarantula/tarantula_test.rb
@@ -1,14 +1,42 @@
 require "#{File.dirname(__FILE__)}/../test_helper"
 require "relevance/tarantula"
 
-class TarantulaTest < ActionController::IntegrationTest
+class TarantulaTestLoggedOut < ActionController::IntegrationTest
   fixtures :all
 
-  def test_tarantula
+  def test_logged_out
     tarantula_crawl(self)
   end
+end
 
-  def test_tarantula_logged_in
+class TarantulaTestLoggedInReadOnly < ActionController::IntegrationTest
+  fixtures :all
+
+  def test_logged_in_read_only
+    post '/user/auth', "user[login]" => 'james', "user[password]" => 'testing'
+    follow_redirect!
+    t = tarantula_crawler(self)
+    t.read_only = true
+    t.crawl "/"
+  end
+end
+
+class TarantulaTestLoggedInNonDestructive < ActionController::IntegrationTest
+  fixtures :all
+
+  def test_logged_in_non_destructive
+    post '/user/auth', "user[login]" => 'james', "user[password]" => 'testing'
+    follow_redirect!
+    t = tarantula_crawler(self)
+    t.non_destructive = true
+    t.crawl "/"
+  end
+end
+
+class TarantulaTestLoggedInAll < ActionController::IntegrationTest
+  fixtures :all
+
+  def test_logged_in_all
     post '/user/auth', "user[login]" => 'james', "user[password]" => 'testing'
     follow_redirect!
     tarantula_crawl(self)
