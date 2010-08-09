a4ff395b934dfbf5eb922c27814bdb31f26e2669
James Smith <james@floppy.org.uk>
2010-08-09 22:36:53 +0100

    change tests to check new group invitations controller

---
+++ test/functional/group_invitations_controller_test.rb
@@ -0,0 +1,53 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'group_invitations_controller'
+
+# Re-raise errors caught by the controller.
+class GroupInvitationsController; def rescue_action(e) raise e end; end
+
+class GroupInvitationsControllerTest < ActionController::TestCase
+
+  fixtures :groups
+
+  def setup
+    @controller = GroupInvitationsController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+    @group = Group.find(2)
+  end
+
+  def when_logged_in
+    @request.session[:user_id] = 1
+    yield
+  end
+
+  def when_not_logged_in
+    @request.session[:user_id] = nil
+    yield
+  end
+
+  def test_invite
+    when_not_logged_in do
+      get :new, :group_id => @group.name
+      assert_response :redirect
+      assert_redirected_to '/'
+    end
+    when_logged_in do
+      get :new, :group_id => @group.name
+      assert_response :success
+      assert_template 'new'
+    end
+  end
+
+  def test_send_invitations
+    when_not_logged_in do
+      post :create, :group_id => @group.name
+      assert_response :redirect
+      assert_redirected_to '/'
+    end
+    when_logged_in do
+      post :create, :group_id => @group.name
+      assert_response :redirect
+      assert_redirected_to @group
+    end
+  end
+end
--- test/functional/groups_controller_test.rb
+++ test/functional/groups_controller_test.rb
@@ -14,40 +14,4 @@ class GroupsControllerTest < ActionController::TestCase
     @response   = ActionController::TestResponse.new
   end
 
-  def when_logged_in
-    @request.session[:user_id] = 1
-    yield
-  end
-  
-  def when_not_logged_in
-    @request.session[:user_id] = nil
-    yield
-  end
-
-  def test_invite
-    @group = Group.find(2)
-    when_logged_in do
-      get :invite, :id => @group.id
-      assert_response :success
-      assert_template 'invite'
-    end
-    when_not_logged_in do
-      get :invite, :id => @group.id
-      assert_response :redirect 
-      assert_redirected_to '/'
-    end
-  end
-
-  def test_send_invitations
-    when_logged_in do
-      post :send_invitations, :id => 2
-      assert_response :redirect
-      assert_redirected_to '/groups/view/2'
-    end
-    when_not_logged_in do
-      post :send_invitations, :id => 2
-      assert_response :redirect
-      assert_redirected_to '/'
-    end
-  end
 end
