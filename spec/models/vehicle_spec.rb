ab3876e278d643c515bfa9da21ea6d70ef479ce5
James Smith <james@floppy.org.uk>
2010-08-09 22:27:26 +0100

    remove old-style emissions test

--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -4,23 +4,9 @@ describe Vehicle do
 
   fixtures :users, :vehicles, :vehicle_fuel_purchases, :vehicle_fuel_types, :vehicle_fuel_classes, :vehicle_fuel_units
 
-  it "old-style emissions calc should calculate emissions for period based on first purchase, not second" do
+  it "emissions calc should calculate emissions for period based on first purchase, not second" do
     # Load correct vehicle from fixtures
     vehicle = vehicles(:vehicle_for_emissions_test)
-    vehicle.user.tester = false
-    # Calculate emissions
-    vehicle.vehicle_fuel_purchases.count.should be(3)
-    emissions = vehicle.emissions
-    # The first emissions period should use 4 litres per day, giving 4 kg of co2 per day
-    emissions[0][:co2_per_day].should be_close(4, 1e-9)
-    # The second emissions period should use 6 litres per day, giving 6 kg of co2 per day
-    emissions[1][:co2_per_day].should be_close(6, 1e-9)
-  end
-
-  it "new-style emissions calc should calculate emissions for period based on first purchase, not second" do
-    # Load correct vehicle from fixtures
-    vehicle = vehicles(:vehicle_for_emissions_test)
-    vehicle.user.tester = true
     # Calculate emissions
     vehicle.vehicle_fuel_purchases.count.should be(3)
     emissions = vehicle.emissions
