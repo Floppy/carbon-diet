dbc9370fcaaf653333af99233b006a8b0475a518
James Smith <james@floppy.org.uk>
2011-10-18 22:39:40 +0100

    fix typo in session store initialiser

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,3 @@
-CarbonDiet::Application.config.session_store 
-  :active_record_store, 
+CarbonDiet::Application.config.session_store :active_record_store, 
   :key => "_carbondiet_session",
   :secret => 'wiVaiDieteGae8thoh0beeQueinie5yahcaeThei8laen3ia2aZahp5aeB6hi2xoolitadi3IeD3Rie5'
