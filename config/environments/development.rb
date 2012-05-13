e9db8d33c6ee147364f48e6bc00957fe6fe62826
James Smith <james@floppy.org.uk>
2012-05-13 23:25:58 +0100

    Turn off mail delivery for dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,12 +15,7 @@ CarbonDiet::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "localhost",
-    :port => 25,
-    :domain => "carbondiet.org",
-  }
+  config.action_mailer.delivery_method = :file
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
