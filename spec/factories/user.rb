b58327344290ad8efaa7b1ccde142927d16395ad
James Smith <james@floppy.org.uk>
2012-05-14 01:17:33 +0100

    Remove all non-alpha chars from fake username

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
 
   factory :user do
-    login { Faker::Internet.user_name }
+    login { Faker::Internet.user_name.gsub('.','').gsub('_','').gsub(' ','') }
   end
 
   factory :user_with_email, :class => 'user' do
-    login { Faker::Internet.user_name.gsub('.','') }
+    login { Faker::Internet.user_name.gsub('.','').gsub('_','').gsub(' ','') }
     email { Faker::Internet.email }
     confirmation_code { SecureRandom.hex }
   end
