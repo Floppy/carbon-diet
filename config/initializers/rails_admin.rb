3a0faeff687f52664a2b1aac74d73987f2aa7aa6
James Smith <james@floppy.org.uk>
2012-05-29 23:02:51 +0100

    Fix rails_admin config and update

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -2,17 +2,25 @@ RailsAdmin.config do |config|
   
   config.current_user_method { get_current_user }
   
-  config.main_app_name = ['Carbon Diet', 'Admin']
-
-  RailsAdmin.config do |config|
-    config.authenticate_with do
-      check_logged_in
-    end
+  config.authenticate_with do
+    check_logged_in
   end
 
-  # Or use simple custom authorization rule:
   config.authorize_with do
     redirect_to root_path unless get_current_user.admin
   end
 
+end
+
+# WillPaginate fix from https://github.com/sferik/rails_admin/issues/816
+if defined?(WillPaginate)
+  module WillPaginate
+    module ActiveRecord
+      module RelationMethods
+        alias_method :per, :per_page
+        alias_method :num_pages, :total_pages
+        alias_method :total_count, :count
+      end
+    end
+  end
 end
