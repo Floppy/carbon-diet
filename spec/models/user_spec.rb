d1d56e4ea2acb4aa132b8e5a0d29b0430ec0d8bf
James Smith <james@floppy.org.uk>
2013-01-15 23:10:51 +0000

    Fix factorygirl warnings

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,27 +3,27 @@ require 'spec_helper'
 describe User do
 
   it "should show an admin icon for administrators" do
-    user = Factory(:user, :admin => true)
+    user = FactoryGirl.create(:user, :admin => true)
     user.admin.should be_true
     user.image.should == 'admin.png'
   end
 
   it "should show a tester icon for testers" do
-    user = Factory(:user, :tester => true)
+    user = FactoryGirl.create(:user, :tester => true)
     user.admin.should be_false
     user.tester.should be_true
     user.image.should == 'tester.png'
   end
 
   it "should show a user icon for users" do
-    user = Factory(:user)
+    user = FactoryGirl.create(:user)
     user.admin.should be_false
     user.tester.should be_false
     user.image.should == 'user.png'
   end
 
   it "should be able to assign email successfully" do
-    user = Factory(:user)
+    user = FactoryGirl.create(:user)
     user.email = "test@example.com"
     user.email.should == "test@example.com"
   end
