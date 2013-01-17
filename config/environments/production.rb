4876698f7b5e0725fafbc57c1745c6d48bab50a8
James Smith <james@floppy.org.uk>
2013-01-17 10:28:28 +0000

    Add css to asset pipeline properly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ CarbonDiet::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( application-default.css application-ie.css )
 
   # Disable delivery errors if you bad email addresses should just be ignored
   config.action_mailer.raise_delivery_errors = false
