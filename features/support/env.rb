b5a507ab9510d5873183cc1a30e69cf6d9fa763d
James Smith <james@floppy.org.uk>
2013-08-03 11:43:13 +0100

    Auto-load fixtures for cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -64,3 +64,10 @@ end
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
+
+Before do
+  ActiveRecord::Fixtures.reset_cache
+  fixtures_folder = File.join(Rails.root, 'spec', 'fixtures')
+  fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
+  ActiveRecord::Fixtures.create_fixtures(fixtures_folder, fixtures)
+end
